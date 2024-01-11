a56b1ad814235d8d92143821a8ad883d9d39c28a
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2024-01-11 15:44:05 +0400

    добавлен stylex и плагины для eslint и vite

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import { resolve } from 'path'
+import stylexPlugin from '@stylexjs/rollup-plugin'
 dotenv.config()
+
+const src = resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +15,14 @@ export default defineConfig({
   define: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    stylexPlugin({
+      dev: true,
+      unstable_moduleResolution: {
+        type: 'commonJS',
+        rootDir: src,
+      },
+    }),
+  ],
 })
