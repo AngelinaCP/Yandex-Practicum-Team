38130175a30af96cbdbfe3b31dd660eb3bde4247
Eugene Kaygorodov <e.kaygorodov@artsofte.ru>
2024-01-13 19:04:33 +0500

    new test suite

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -8,7 +8,7 @@ global.fetch = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve('hey') })
 )
 
-test('Example test', async () => {
+test('it has 8 routes', async () => {
   render(<App />)
-  expect(screen.getByText(appContent)).toBeDefined()
+  expect(screen.getAllByRole('link')).toHaveLength(8)
 })
