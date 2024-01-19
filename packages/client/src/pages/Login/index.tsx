beb93f5b5986677fa8e6aa1bfc1bc6d41e6fee99
Vsevolod <demidov.vsevolod@uep24.ru>
2024-01-19 21:39:06 +0300

    add Login page

---
+++ packages/client/src/pages/Login/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Card from '@/components/Card'
+import Input from '@/components/Input'
+import Button from '@/components/Button/index'
+import Link from '@/components/Link'
+import { StyledForm } from '@/pages/Login/style'
+
+const LogIn = () => {
+  const navigate = useNavigate()
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    // TODO:
+    // @ts-ignore
+    console.log(
+      'login: ',
+      event.currentTarget[0].value,
+      '     password: ',
+      event.currentTarget[1].value
+    )
+    navigate('/')
+  }
+
+  return (
+    <Card height={'400px'} width={'400px'}>
+      <StyledForm onSubmit={handleSubmit}>
+        <Input name={'Логин'} required={true} />
+        <Input type={'password'} name={'Пароль'} required={true} />
+        <Button type={'submit'} $primary={true}>
+          {' '}
+          войти{' '}
+        </Button>
+        <Link to={'/signup'}> регистрация </Link>
+      </StyledForm>
+    </Card>
+  )
+}
+
+export default LogIn
---
+++ packages/client/src/pages/Login/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const StyledForm = styled.form`
+  display: flex;
+  margin: 50px 0;
+  flex-direction: column;
+  align-items: center;
+`
