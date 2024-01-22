53647d0a20444db97f3ccab59cbf4364c626eedc
Vsevolod <demidov.vsevolod@uep24.ru>
2024-01-22 14:42:34 +0300

    add 404 page

---
+++ packages/client/src/pages/Page_404/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from '@/components/Link'
+import { StyledError } from '@/pages/Page_404/style'
+
+const Page404 = () => (
+  <StyledError>
+    <span>404</span>
+    <span>не туда попали</span>
+    <Link to={'/'}>вернуться на главную</Link>
+  </StyledError>
+)
+
+export default Page404
