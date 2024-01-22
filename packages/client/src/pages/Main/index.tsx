bcf5fa36fbc9d79e35264de8a062503425630198
Vsevolod <demidov.vsevolod@uep24.ru>
2024-01-22 12:54:44 +0300

    add mine page

---
+++ packages/client/src/pages/Main/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react'
+import Logo from '@/components/Logo'
+import Card from '@/components/Card'
+import ButtonLink from '@/components/ButtonLink'
+import {
+  StyledBG,
+  StyledInnerWrapper,
+  StyledMainWrapper,
+} from '@/pages/Main/style'
+
+const MainPage: FC = () => {
+  return (
+    <StyledMainWrapper>
+      <Logo />
+      <StyledInnerWrapper>
+        <Card height={'300px'} width={'300px'}>
+          <ButtonLink to={'/game'} $primary={true}>
+            играть
+          </ButtonLink>
+          <ButtonLink to={'/profile'}>профиль</ButtonLink>
+          <ButtonLink to={'/leaderboard'}>лидеры</ButtonLink>
+          <ButtonLink to={'/forum'}>форум</ButtonLink>
+        </Card>
+        <StyledBG />
+        <p>
+          В этой игре нужно бежать, преодолевать препятствия, и пробежать дальше
+          всех.
+        </p>
+      </StyledInnerWrapper>
+    </StyledMainWrapper>
+  )
+}
+
+export default MainPage
