706a224cc64554a629934d6b5ba695b3db511b6b
Vsevolod <demidov.vsevolod@uep24.ru>
2024-01-22 11:56:52 +0300

    style fix

--- packages/client/src/pages/Login/style.ts
+++ packages/client/src/pages/Login/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const StyledForm = styled.form`
   display: flex;
-  margin: 50px 0;
+  margin: 20px 0;
   flex-direction: column;
   align-items: center;
 `
