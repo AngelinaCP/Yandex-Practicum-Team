1fa032d983f73ddf9c09022b31520a8efd9a5a07
Vsevolod <demidov.vsevolod@uep24.ru>
2024-01-23 14:34:27 +0300

    fix 500 page

--- packages/client/src/pages/Page_500/index.tsx
+++ packages/client/src/pages/Page_500/index.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import Link from '@/components/Link'
 import { StyledError } from '@/pages/Page_500/style'
 
-const Page404 = () => (
+export const Page500 = () => (
   <StyledError>
     <span>500</span>
     <span>мы уже фиксим</span>
     <Link to={'/'}>вернуться на главную</Link>
   </StyledError>
 )
-
-export default Page404
