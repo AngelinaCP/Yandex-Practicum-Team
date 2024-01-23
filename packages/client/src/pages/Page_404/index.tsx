c7c521d81f7797fee2ff666d3617b73f010771b2
Vsevolod <demidov.vsevolod@uep24.ru>
2024-01-23 14:40:19 +0300

    add named export

--- packages/client/src/pages/Page_404/index.tsx
+++ packages/client/src/pages/Page_404/index.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import Link from '@/components/Link'
 import { StyledError } from '@/pages/Page_404/style'
 
-const Page404 = () => (
+export const Page404 = () => (
   <StyledError>
     <span>404</span>
     <span>не туда попали</span>
     <Link to={'/'}>вернуться на главную</Link>
   </StyledError>
 )
-
-export default Page404
