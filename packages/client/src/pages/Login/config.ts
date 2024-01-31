c53123ca1fe519eaeb62290d2a2eac3425fd6cb6
Vsevolod <demidov.vsevolod@uep24.ru>
2024-01-31 13:59:18 +0300

    config fix

--- packages/client/src/pages/Login/config.ts
+++ packages/client/src/pages/Login/config.ts
@@ -3,8 +3,8 @@ import { object, string, TypeOf } from 'zod'
 const loginSchema = object({
   login: string()
     .min(1, 'login is required')
-    .min(3, 'login must be more than 8 characters')
-    .max(8, 'login must be less than 32 characters'),
+    .min(8, 'login must be more than 8 characters')
+    .max(32, 'login must be less than 32 characters'),
   password: string()
     .min(1, 'Password is required')
     .min(8, 'Password must be more than 8 characters')
