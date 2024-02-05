63ab6ad55acc31ca83adfbd5825589fac9567218
5everD <48820363+5everD@users.noreply.github.com>
2024-02-05 22:03:41 +0300

    style fix (#36)
    Co-authored-by: Vsevolod <demidov.vsevolod@uep24.ru>

--- packages/client/src/pages/Profile/style.ts
+++ packages/client/src/pages/Profile/style.ts
@@ -8,21 +8,8 @@ export const StyledForm = styled.form`
 
 export const StyledFormGroup = styled.div`
   display: flex;
-  width: 100%;
   flex-direction: row;
   gap: 80px;
-
-  button {
-    margin-bottom: 0;
-    margin-top: 18px;
-  }
-`
-
-export const StyledFormGroupModal = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-  gap: 20px;
 
   button {
     margin-bottom: 0;
