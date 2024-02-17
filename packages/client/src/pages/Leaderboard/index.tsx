5b3adb0b856793cd352e6febe9bf07a748cea021
Eugene Kaygorodov <e.kaygorodov@artsofte.ru>
2024-02-17 18:31:01 +0500

    YAN-73: loader fix

--- packages/client/src/pages/Leaderboard/index.tsx
+++ packages/client/src/pages/Leaderboard/index.tsx
@@ -6,12 +6,14 @@ import {
   StyledTh,
   StyledTitle,
 } from '@/pages/Leaderboard/style'
+import { LoaderSpinner } from '@/components/Loading'
+import { Fragment } from 'react'
 
 export const LeaderboardPage = () => {
   const { data, isLoading } = useGetLeaderboardQuery(null)
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <LoaderSpinner />
   }
 
   return (
@@ -22,17 +24,15 @@ export const LeaderboardPage = () => {
         <StyledTh>Очки</StyledTh>
         <StyledTh>Дата</StyledTh>
         {data &&
-          data.map(item => {
-            return (
-              <>
-                <StyledTd>{item.data.name}</StyledTd>
-                <StyledTd>{item.data.silentHillScore}</StyledTd>
-                <StyledTd>
-                  {new Date(item.data.date).toLocaleDateString()}
-                </StyledTd>
-              </>
-            )
-          })}
+          data.map(item => (
+            <Fragment key={String(item.data.date)}>
+              <StyledTd>{item.data.name}</StyledTd>
+              <StyledTd>{item.data.silentHillScore}</StyledTd>
+              <StyledTd>
+                {new Date(item.data.date).toLocaleDateString()}
+              </StyledTd>
+            </Fragment>
+          ))}
       </StyledGrid>
     </StyledLayout>
   )
