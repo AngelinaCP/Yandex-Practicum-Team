433eec08aba123941822d7236b05eb1f29ac61e8
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2024-03-25 04:18:38 +0400

    fix test
    - add redux provider and store to test ?
    - wrap async fetch in try in App.tsx

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -26,9 +26,13 @@ function App() {
   useEffect(() => {
     const fetchServerData = async () => {
       const url = `http://localhost:${__SERVER_PORT__}/api`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+      } catch (err) {
+        console.log('server api request failed')
+      }
     }
 
     fetchServerData()
